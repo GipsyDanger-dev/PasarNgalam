b355ad220bcca7a578fb7676340ad95366567ed2
sharkjira99@gmail.com <sharkjira99@gmail.com>
2025-12-13 23:29:44 +0700

    mbut8

--- app/Events/DriverLocationUpdated.php
+++ app/Events/DriverLocationUpdated.php
@@ -6,8 +6,7 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\ShouldBroadcast;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
